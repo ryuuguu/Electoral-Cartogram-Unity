0006f692b11d23c651e361d404ce2a9754b811fd
Grant Morgan <grant@ryuuguu.com>
2020-11-01 17:33:37 +0900

    added test screenShot GO to code

---
+++ Assets/Scripts/SubCells/Utils/ScreenShotTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShotTest : MonoBehaviour {
+
+    public bool takeShot = false;
+    public string fileName = "testScreenShot";
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (takeShot) {
+            takeShot = false;
+            ScreenCapture.CaptureScreenshot(fileName);
+            Debug.Log("ScreenShotTest");
+        } 
+    }
+}
